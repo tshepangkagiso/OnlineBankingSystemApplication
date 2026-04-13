ef12d495d747f1f6e76064f64020e5746031c4e4
tshepangkagiso <tshepangkagisomashigo8@gmail.com>
2026-04-13 23:59:02 +0200

    frontend: cleanup

--- online-banking-system-frontend/src/app/app.ts
+++ online-banking-system-frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('online-banking-system-frontend');
+  protected readonly title = signal('online banking system');
 }
