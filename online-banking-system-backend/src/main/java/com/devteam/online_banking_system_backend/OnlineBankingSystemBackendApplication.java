3beae4c2be528c933cfd0425ad7c93d862a402f9
tshepangkagiso <tshepangkagisomashigo8@gmail.com>
2026-04-13 23:53:43 +0200

    backend init

---
+++ online-banking-system-backend/src/main/java/com/devteam/online_banking_system_backend/OnlineBankingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devteam.online_banking_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingSystemBackendApplication.class, args);
+	}
+
+}
---
+++ online-banking-system-backend/src/test/java/com/devteam/online_banking_system_backend/OnlineBankingSystemBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.devteam.online_banking_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(locations = "file:./.env")
+class OnlineBankingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
